f646ce0f0e53726c6cc0442a32b4c5c5497987c7
kolesnicknick <mkolis@softserveinc.com>
2020-03-02 23:49:51 +0200

    Update app.js

--- chucknorris/app.js
+++ chucknorris/app.js
@@ -9,7 +9,7 @@ function getJokes(e) {
     let number = document.querySelector("input[type='number']").value;
 
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+    xhr.open('GET', `https://api.icndb.com/jokes/random/${number}`, true);
 
     xhr.onload = function(){
       if(this.status === 200){
